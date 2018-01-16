f2f10fe0e73227a835e49f3aacca9cae64ebc913
amol <akiajk@nus.edu.sg>
2018-01-16 17:07:54 +0800

    Face Selection Methods Added

--- src/app/gs-viewer/viewer/viewer.component.ts
+++ src/app/gs-viewer/viewer/viewer.component.ts
@@ -39,6 +39,7 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
   
   mySprites: THREE.Sprite[] = [];
   scenechildren:Array<any>;
+  faceBoundary:THREE.Line;
 
   constructor(injector: Injector, myElement: ElementRef) { 
     super(injector);
@@ -80,7 +81,6 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     this.selecting = this.dataService.getselecting();  // todo: should this be in the data service??
     this.mouse = new THREE.Vector2();
     this.raycaster = new THREE.Raycaster();
-    this.scenechildren=this.getSceneChildren();
     
     //Material of select and basic;
     for(var i=0;i<this.scene.children.length;i++){
@@ -96,7 +96,7 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     let self = this;
     function animate() {
         self.raycaster.setFromCamera(self.mouse,self.camera);
-        var intersects = self.raycaster.intersectObjects(self.scenechildren);
+        var intersects = self.raycaster.intersectObjects(self.scenechildren, true);
         for (var i = 0; i < self.scenechildren.length; i++) {
           var currObj=self.scenechildren[i];
           if(self.dataService.getSelectingIndex(currObj.uuid)<0) {
@@ -107,9 +107,7 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
             }
           }
         }
-        if(self.dataService.selecting.length!=0){
-          self.updateview();
-      }
+        self.updateview();
 
       requestAnimationFrame( animate );
       self.renderer.render( self.scene, self.camera );
@@ -124,7 +122,6 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     if(message == "model_update"){
       this.updateModel();
     }
-    
   }
 
 
@@ -138,21 +135,18 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     }
   }
 
-
   //
   // update mode
   // todo: optimize
   // 
   updateModel(): void{
-
     this._model = this.dataService.getGsModel(); 
-
     if( !this._model || !this.scene){
       console.warn("Model or Scene not defined");
       return;
     }
-
     try{
+      this.scenechildren=[];
       const scene_data: gs.IThreeScene = gs.genThreeModel( this._model );
 
       //[three_mode, egde_map, tri_map] = genThreeModelandMaps()
@@ -160,20 +154,18 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
       //gs.getThreeFace(label)
       //gs.getThreeObj
 
-      
       this.clearScene();
-
       let loader = new THREE.ObjectLoader();
-
       let objectData = loader.parse( scene_data );
       for(var i =0;i< objectData.children.length;i++){
         if( objectData.children[i].children!==undefined){
           for(var j=0;j< objectData.children[i].children.length;j++){
             let chd = objectData.children[i].children[j];
-            if( chd.type==="Mesh"||chd.type==="LineLoop"||chd.type==="LineSegments"||chd.type==="Line"){
-               objectData.children[i].children[j]["geometry"].computeVertexNormals();
-               objectData.children[i].children[j]["geometry"].computeBoundingBox();
-               objectData.children[i].children[j]["geometry"].computeBoundingSphere();
+            if( chd.type==="Mesh"){
+               chd["geometry"].computeVertexNormals();
+               chd["geometry"].computeBoundingBox();
+               chd["geometry"].computeBoundingSphere();
+               this.scenechildren.push(chd);
             }
             /// 
             if( chd.children.length > 0){
@@ -187,6 +179,7 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
         }
       }
       this.scene.add(objectData);
+      this.createNewFaceBoundary();
     }
     catch(ex){
       console.error("Error displaying model:", ex);
@@ -219,24 +212,38 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
   /// selects object from three.js scene
   onDocumentMouseDown(event){
     event.preventDefault();
-    var selectedObj, intersects;
+    var selectedObj;
+    var intersects=this.getInteresects();
     this.raycaster.setFromCamera(this.mouse,this.camera);
-    intersects = this.raycaster.intersectObjects(this.scenechildren);
+    intersects = this.raycaster.intersectObjects(this.scenechildren, true);
     if ( intersects.length > 0 ) {
-      selectedObj=intersects[ 0 ].object;
-      console.log(intersects);
-      var index=this.dataService.getSelectingIndex(selectedObj.uuid);
-      if(index<0) {
-        selectedObj.material=this.selectMat;
-        this.dataService.pushselecting(selectedObj);
-      } else {
-        selectedObj.material=this.basicMat;
-        this.dataService.spliceselecting(index,1);
-        for(var i=0;i<this.dataService.sprite.length;i++){
-        this.dataService.sprite[i].visible=false;
+      if(this.dataService.visible=="Faces") {
+        var index=this.dataService.getFaceIndex(intersects[ 0 ].faceIndex);
+        if(index>=0) {
+          this.scene.remove(this.dataService.selectedFaces[index]);
+          this.dataService.selectedFaces.splice(index,1);
+        } else {
+          this.updateFaceBoundary(intersects[0]);
+          this.faceBoundary.name=intersects[0].faceIndex;
+          this.faceBoundary.material=this.selectedMat;
+          this.dataService.selectedFaces.push(this.faceBoundary);
+          this.createNewFaceBoundary();
         }
-        var sprite=[];
-        this.dataService.pushsprite(sprite);
+      } else {
+        selectedObj=intersects[ 0 ].object;
+        var index=this.dataService.getSelectingIndex(selectedObj.uuid);
+        if(index<0) {
+          selectedObj.material=this.selectMat;
+          this.dataService.pushselecting(selectedObj);
+        } else {
+          selectedObj.material=this.basicMat;
+          this.dataService.spliceselecting(index,1);
+          for(var i=0;i<this.dataService.sprite.length;i++){
+            this.dataService.sprite[i].visible=false;
+          }
+          var sprite=[];
+          this.dataService.pushsprite(sprite);
+        }
       }
     } else {
       for(var i=0;i<this.dataService.selecting.length;i++){
@@ -253,10 +260,46 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     //this.updateview();
   }
 
+  selectedMat:any=new THREE.LineBasicMaterial( { color: 0x0000ff, linewidth: 2, transparent: true } );
+  mouseHovMat:any=new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2, transparent: true } );
+  createNewFaceBoundary() {
+    var geometry = new THREE.BufferGeometry();
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( 4 * 3 ), 3 ) );
+    var material = this.mouseHovMat;
+    this.faceBoundary=new THREE.Line( geometry, material );
+    this.scene.add( this.faceBoundary );
+  }
+
+  updateFaceBoundary(obj) {
+    var face=obj.face;
+    var linePosition = this.faceBoundary.geometry.attributes.position;
+    var meshPosition = obj.object.geometry.attributes.position;
+    linePosition.copyAt( 0, meshPosition, face.a );
+    linePosition.copyAt( 1, meshPosition, face.b );
+    linePosition.copyAt( 2, meshPosition, face.c );
+    linePosition.copyAt( 3, meshPosition, face.a );
+    obj.object.updateMatrix();
+    this.faceBoundary.geometry.applyMatrix( obj.object.matrix );
+    this.faceBoundary.visible = true;
+  }
+
+  getInteresects() {
+    var intersects=[];
+    this.raycaster.setFromCamera(this.mouse,this.camera);
+    var objsArray = this.raycaster.intersectObjects(this.scenechildren[0], true);
+    for(var i=0;i<objsArray.length;i++) {
+      intersects[i]
+    }
+    return intersects;
+  }
+
   updateview(){
     this.Visible=this.dataService.visible;
-    var intersects = this.raycaster.intersectObjects(this.scenechildren);
+    var intersects = this.raycaster.intersectObjects(this.scenechildren, true);
     if ( intersects.length > 0 ) {
+      if(this.dataService.visible=="Faces") {
+        this.updateFaceBoundary(intersects[0]);
+      } 
       if(this.dataService.selecting.length!=0){
         for(var i=0;i<this.mySprites.length;i++){
           if(this.mySprites[i].parent.name===this.Visible){
@@ -288,27 +331,27 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
     }
   }
 
-  getSceneChildren() {
-    var scenechildren=[];
-    var children;
-    for (var i = 0; i<this.scene.children.length; i++) {
-      if(this.scene.children[i].name=="Scene") {
-        children=this.scene.children[i].children;
-        break;
-      }
-      if(i==this.scene.children.length-1) {
-        return [];
-      }
-    }
-    for(var i=0;i<children.length;i++){
-      for(var j=0;j<children[i].children.length;j++){
-        if(children[i].children[j].type==="Mesh"||children[i].children[j].type==="Line"){
-          scenechildren.push(children[i].children[j]);
-        }
-      }
-    }
-    return scenechildren;
-  }
+  // getSceneChildren() {
+  //   var scenechildren=[];
+  //   var children;
+  //   for (var i = 0; i<this.scene.children.length; i++) {
+  //     if(this.scene.children[i].name=="Scene") {
+  //       children=this.scene.children[i].children;
+  //       break;
+  //     }
+  //     if(i==this.scene.children.length-1) {
+  //       return [];
+  //     }
+  //   }
+  //   for(var i=0;i<children.length;i++){
+  //     for(var j=0;j<children[i].children.length;j++){
+  //       if(children[i].children[j].type==="Mesh"){
+  //         scenechildren.push(children[i].children[j]);
+  //       }
+  //     }
+  //   }
+  //   return scenechildren;
+  // }
 
   zoomfit(){
     //if(this.selecting.length===0){
@@ -365,8 +408,6 @@ export class ViewerComponent extends DataSubscriber implements OnInit {
       this.controls.target.set(newLookAt.x, newLookAt.y,newLookAt.z);
     }*/
   }
-
-
   /*render():void {
     let self = this;
     (function render(){
